82ef83d391b3548490d9fe703871f80adf8f80ba
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-06-28 16:45:14 -0400

    Discord Bot Test

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.use((req, res, next) =>
     );
     next();
 });
+
 /*
 // If we're hosted on the server, it's best to set a "static" folder. For this case,
 // we'll bind the frontend/build static folder, so we attempt to serve that to the
