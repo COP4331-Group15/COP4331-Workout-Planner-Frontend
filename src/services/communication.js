804ef1a5dc6e88a30f60b4216402178eb381ce8a
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-07-02 11:36:52 -0400

    Hopefully fix the endpoint for real this time

--- src/services/communication.js
+++ src/services/communication.js
@@ -3,7 +3,7 @@ import fire from "./fire";
 
 var url = "http://localhost:5000/api";
 if(process.env.NODE_ENV === 'production') {
-    url = 'https://workout-sprinter-api.herokuapp.com/api';
+    url = 'http://workout-sprinter-api.herokuapp.com/api';
 }
 
 const createToken = async () => {
