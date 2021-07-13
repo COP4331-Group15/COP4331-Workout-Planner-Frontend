98c5a9e7dda7f3e6d967e9438e3ee03f41421f66
rickyegawa <84685882+rickyegawa@users.noreply.github.com>
2021-07-13 00:42:47 -0700

    Add files via upload

---
+++ Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+    return (
+      <>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to='/'>Workout Planner</NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to ='about'>About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to ='discover'>Discover</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to ='signup'>Sign Up</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>
+        </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar;
