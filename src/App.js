37ef465bfe7ddd3a368da121dd2651b513c3ac73
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-07-14 16:42:54 -0400

    Moved another set of files

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import fire from './services/fire';
-
-import TestPage from './pages/TestPage';
-import Login from './pages/Login';
-import SignUp from './pages/Signup';
+import Home from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
-
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  fire.auth().onAuthStateChanged((user) => {
-    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
-  });
-
-  console.log('logged in?', isLoggedIn);
-
-  const signOut = () => {
-    fire.auth().signOut();
-  }
-
   return (
     <Router>
-      {!isLoggedIn ? (
-        <>
-          <Switch>
-            <Route path="/signup">
-              <SignUp />
-            </Route>
-            <Route path="/">
-              <Login />
-            </Route>
-          </Switch>
-        </>
-      ) : (
-        <>
-          <span onClick={signOut}>
-            <a href="#">Sign Out</a>
-          </span>
-          <Switch>
-            <Route path="/">
-              <TestPage />
-            </Route>
-          </Switch>
-        </>
-      )}
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SigninPage} exact />       
+      </Switch>
+
     </Router>
   );
 }
