88224861dee624086f5e1da9c64415614fad04fd
Ricky Egawa <egawaricky@gmail.com>
2021-08-01 19:26:32 -0400

    Newest UI up-to-date 8/1/21

--- src/services/communication.js
+++ src/services/communication.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import fire from "./Firebase";
+import firebase from "firebase";
 
 const url = 'https://workout-sprinter-api.herokuapp.com/api';
 
 const createToken = async () => {
-    const user = fire.auth().currentUser;
+    const user = firebase.auth().currentUser;
     const token = user && (await user.getIdToken());
 
     const payloadHeader = {
@@ -24,6 +24,96 @@ export const getTestData = async () => {
     try {
         const res = await axios.get(url + "/test", header);
         return res.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const getCalendarData = async (year, month) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+
+    try {
+        const res = await axios.get(url + "/calendar/" + uuid + "/" + year + "/" + month, header);
+        return res.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const postWorkoutData = async (workout) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+
+    try{
+        const res = await axios.post(url + "/workout/" + uuid + "/create", workout, header);
+        return res.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const postExerciseData = async (exercise) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+
+    try{
+        const res = await axios.post(url + "/exercise/" + uuid + "/create", exercise, header);
+        return res.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const patchExerciseData = async (exercise, exerciseId) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+    console.log(exerciseId);
+    // const exerciseID = firebase.auth().currentUser.exerciseId;
+
+    try{
+        console.log(url + "/exercise/" + uuid + "/" + exerciseId);
+        const res = await axios.patch(url + "/exercise/" + uuid + "/" + exerciseId + "/update", exercise, header);
+        return;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const patchCalendarData = async (year, month, day, workout) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+
+    try {
+        const res = await axios.patch(url + "/calendar/" + uuid + "/" + year + "/" + month + "/" + day + "/update", workout, header);
+        return;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const deleteCalendarData = async (year, month, day, workout) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+
+    try {
+        const res = await axios.patch(url + "/calendar/" + uuid + "/" + year + "/" + month + "/" + day + "/update", workout, header);
+        return;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const deleteExerciseData = async (exerciseId) => {
+    const header = await createToken();
+    const uuid = firebase.auth().currentUser.uid;
+    console.log(exerciseId);
+    // const exerciseID = firebase.auth().currentUser.exerciseId;
+
+    try{
+        console.log(url + "/exercise/" + uuid + "/" + exerciseId);
+        const res = await axios.delete(url + "/exercise/" + uuid + "/" + exerciseId + "/delete", header);
+        return;
     } catch (e) {
         console.error(e);
     }
